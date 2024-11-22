dbb012415e104fe12911e4e486d649afeb85bdd0
nemanja <antonijevicnemanja68@gmail.com>
2024-11-22 14:46:33 +0100

    blog card component

--- src/app/(visitorsFacing)/_components/BlogCard.tsx
+++ src/app/(visitorsFacing)/_components/BlogCard.tsx
@@ -1,12 +1,42 @@
 "use client"
-import Image from "next/image";
+import { db } from "@/firebaseconfig";
+import { collection, query, orderBy, getDocs } from "firebase/firestore";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-export default function BlogCard(title: any,desc: any,image:any){
+export default function BlogCard(){
+
+  const [postList, setPostList] = useState<any[]>([]);
+  const postCollectionRef = collection(db, "posts")
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const q = query(postCollectionRef, orderBy("createdAt", "desc"));
+      const data = await getDocs(q);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  })
+
     return(
-        <div className="w-1/3 h-72 flex-row">
-            <div>{title}</div>
-            <div><Image src={image} width={300} height={300} alt="" /></div>
-            <div>{desc}</div>
-        </div>
+        <div className="flex-row justify-evenly mt-6">
+        {postList.map((post, index) => (
+          <Link key={uuidv4()} href={'/'}>
+            <div className="scale-90 flex-row w-full h-[320px] shadow-md rounded-2xl cursor-pointer">
+            <div className="flex justify-center mt-3">
+              <img className="rounded-xl w-[90%] h-60" src={post.imgUrl} alt="" />
+            </div>
+            <div className="flex justify-center mb-2">
+              <h1 className="text-xl font-semibold">{post.title}</h1>
+            </div>
+            <div className="flex justify-center font-sans">
+              <h3>{post.desc}</h3>
+            </div>
+          </div>
+          </Link>
+        ))}
+      </div>
     )
 }
