dab8f9f5628a18a83c263d9703b5597d24843769
nemanja <antonijevicnemanja68@gmail.com>
2024-11-23 00:06:02 +0100

    landing page

--- src/app/(visitorsFacing)/_components/heading/Heading.tsx
+++ src/app/(visitorsFacing)/_components/heading/Heading.tsx
@@ -1,6 +1,8 @@
+import { ArrowRight, Github, Linkedin } from "lucide-react";
+
 export default function Heading(){
     return(
-        <div className="flex-row w-full mt-20">
+        <div className="flex-row w-full mt-32 border-b border-gray-800">
             <div className="flex md:justify-start justify-center mb-6">
                 <h1 className="font-semibold text-2xl">Welcome to my blog!</h1>
             </div>
@@ -10,8 +12,16 @@ export default function Heading(){
             <div className="flex md:justify-start justify-center mb-6">
                 <p>Thanks for visiting my website.</p>
             </div>
-            <div className="flex md:justify-start justify-center">
+            <div className="flex md:justify-start justify-center mb-6">
                 <p>On this site you can read my articles, follow <b>news and projects from programming.</b></p>
+            </div>
+            <div className="flex md:justify-start gap-4 justify-center mb-6">
+                <button className="btn btn-active btn-neutral btn-sm">View Posts <ArrowRight size={16} /></button>
+                <button className="btn btn-active btn-neutral btn-sm">Contact me</button>
+            </div>
+            <div className="flex md:justify-start justify-center gap-4 mb-6">
+                <a href="https://github.com/Nemanja0033"><Github color="gray" size={20} /></a>
+                <a href="https://www.linkedin.com/in/nemanja-antonijevic-6baabb2a2/"><Linkedin color="gray" size={20} /></a>
             </div>
         </div>
     )
--- src/app/(visitorsFacing)/page.tsx
+++ src/app/(visitorsFacing)/page.tsx
@@ -7,12 +7,12 @@ export default function home(){
 
 
   return(
-    <div className="md:flex flex-row justify-center">
+    <div className="md:flex flex-row justify-center gap-6">
       <div className="md:w-1/2 w-full">
         <Heading />
       </div>
       <div className="h-screen overflow-hidden mt-20 border-l border-gray-800">
-        <h1 className="flex justify-center items-center gap-1 textarea-md">Recent Blogs <ArrowDown size={16} /></h1>
+        <h1 className="flex justify-center items-center gap-1 textarea-md">Recent Posts <ArrowDown size={16} /></h1>
         <BlogCard />
       </div>
     </div>
