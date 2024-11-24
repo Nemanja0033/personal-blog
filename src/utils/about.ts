91f9d371a4d2d91410fd789db0848f436ff47147
nemanja <antonijevicnemanja68@gmail.com>
2024-11-24 14:32:13 +0100

    about page

---
+++ src/utils/about.ts
@@ -0,0 +1,17 @@
+export const about = {
+    aboutMe: "Hello! Im Nemanja, a certified frontend developer and coding enthusiast passionate about creating modern, user-friendly web applications. I am always open to new job opportunities and challenges that allow me to grow and contribute to innovative projects.I have an advanced understanding of JavaScript and TypeScript, which I use to build clean, maintainable, and visually appealing applications. I stay up-to-date with the latest frontend technologies and actively improve my skills",
+    certificate1: {
+        link: 'https://www.credential.net/b135fc92-2dd1-4736-8282-c69012125472',
+        name: 'JavaScript Specialist Certification',
+        text: 'As a certified JavaScript Specialist, I have honed my skills and deepened my understanding of one of the most essential programming languages for web development.',
+    },
+    certificate2: {
+        link: 'https://www.link-group.eu/certificate/',
+        name: 'ITAcademy Frontend JavaScript Developer',
+        text: 'I successfully completed an intensive one-year Frontend Development course with ITAcademy, which provided me with a thorough understanding of modern web development technologies. The course focused on advanced JavaScript techniques, along with essential frontend frameworks and libraries like React. Through practical projects and hands-on experience, I mastered the art of building dynamic, responsive, and user-friendly interfaces.'
+    },
+    contact: {
+        email: 'antonijevicnemanjaa@gmail.com',
+        linkedin: 'https://www.linkedin.com/in/nemanja-antonijevic-6baabb2a2/',
+    }
+}
