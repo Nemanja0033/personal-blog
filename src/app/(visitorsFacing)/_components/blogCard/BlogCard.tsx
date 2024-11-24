6b06c89e0d338ea74c1fbe1a8c93893e72c9a9d6
nemanja <antonijevicnemanja68@gmail.com>
2024-11-24 14:45:39 +0100

    eslint off

--- src/app/(visitorsFacing)/_components/blogCard/BlogCard.tsx
+++ src/app/(visitorsFacing)/_components/blogCard/BlogCard.tsx
@@ -7,7 +7,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function BlogCard(){
 
-  const [postList, setPostList] = useState<any[]>([]);
+  const [postList, setPostList] = useState<any []>([]);
   const postCollectionRef = collection(db, "posts")
   const [loading, setLoading] = useState<boolean>(true);
 
