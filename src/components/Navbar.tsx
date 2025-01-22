1957cb8f69da84580205923eedfc47b827dedfa5
nemanja <antonijevicnemanja68@gmail.com>
2025-01-22 23:30:54 +0100

    FEAT:: adding avatar to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ export default function Navbar(){
     return(
         <nav className="w-full flex justify-evenly h-14 shadow-md">
             <div className="flex items-center">
+               <img className="w-12" src='./avatar.png' />
                <Link href={'/'}><span className="font-bold text-md cursor-pointer text-primary">myBlog</span></Link>
             </div>
             <div className="flex justify-center md:mr-12 mr-0 items-center gap-8 text-sm">
