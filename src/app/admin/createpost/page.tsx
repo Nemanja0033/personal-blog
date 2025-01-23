c8dba16dacd0a43a1d0962615008156d4d1809f3
nemanja <antonijevicnemanja68@gmail.com>
2025-01-23 15:29:21 +0100

    REAFACTOR:: improve readability

--- src/app/admin/createpost/page.tsx
+++ src/app/admin/createpost/page.tsx
@@ -37,12 +37,39 @@ export default function CreatePost(){
         <div className="w-full h-screen justify-self-center mt-32 flex justify-center md:items-center items-start md:mt-0 shadow-md rounded-md">
             <div className="flex-row md:w-[80%] w-[95%]">
                 <h1 className="text-center mb-5 text-2xl font-semibold">New Blog Post</h1>
-                <input className="w-full mb-5 h-10 rounded-md border" required placeholder="Title. . ." type="text" onChange={(e) => {setTitle(e.target.value)}} />
-                <input className="w-full mb-5 h-10 rounded-md border" required placeholder=" Image URL" type="text" onChange={(e) => {setImageUrl(e.target.value)}} />
-                <input className="w-full mb-5 h-10 rounded-md border" required placeholder=" Short Description. . ." type="text" onChange={(e) => {setDesc(e.target.value)}} />
-                <input className="w-full mb-5 h-10 rounded-md border" required placeholder=" Tag. . ." type="text" onChange={(e) => {setTag(e.target.value)}} />
-                <textarea className="w-full mb-5 h-32 max-h-92 rounded-md border" onChange={(e) => setContent(e.target.value)} placeholder="Enter your content. . ." />
-                <button className="w-full h-12 btn btn-active btn-neutral bg-primary text-neutral hover:text-white btn-sm" onClick={createPost}>Submit</button>
+                <input className="w-full mb-5 h-10 rounded-md border" 
+                        required 
+                        placeholder="Title. . ." 
+                        type="text" 
+                        onChange={(e) => {setTitle(e.target.value)}} 
+                        />
+                <input className="w-full mb-5 h-10 rounded-md border" 
+                       required 
+                       placeholder=" Image URL" 
+                       type="text" 
+                       onChange={(e) => {setImageUrl(e.target.value)}} 
+                       />
+                <input className="w-full mb-5 h-10 rounded-md border" 
+                       required 
+                       placeholder=" Short Description. . ." 
+                       type="text" 
+                       onChange={(e) => {setDesc(e.target.value)}} 
+                       />
+                <input className="w-full mb-5 h-10 rounded-md border" 
+                       required 
+                       placeholder=" Tag. . ." 
+                       type="text" 
+                       onChange={(e) => {setTag(e.target.value)}} 
+                       />
+                <textarea className="w-full mb-5 h-32 max-h-92 rounded-md border" 
+                          onChange={(e) => setContent(e.target.value)} 
+                          placeholder="Enter your content. . ." 
+                          />
+                <button 
+                        className="w-full h-12 btn btn-active btn-neutral bg-primary text-neutral hover:text-white btn-sm" 
+                        onClick={createPost}>
+                        Submit
+                </button>
             </div>
         </div>
     )
