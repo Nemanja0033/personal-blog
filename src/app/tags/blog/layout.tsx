28efe35508febf3f087599fe4c6298d461739988
nemanja <antonijevicnemanja68@gmail.com>
2025-02-24 12:56:23 +0100

    patch

---
+++ src/app/tags/blog/layout.tsx
@@ -0,0 +1,21 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export const dynamic = "force-dynamic"
+
+export default function BlogTagDynamic({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body>
+          <Navbar />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    );
+  }
+  
--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,14 +1,27 @@
-"use client"
-import { db } from "@/lib/firebaseconfig";
-import { collection, getDocs, query } from "firebase/firestore";
-import { useEffect, useState } from "react"
+import { db } from "@/lib/firebaseconfig"
+import { collection, getDocs } from "firebase/firestore"
+import Link from "next/link";
 
-export default function tags(){
+async function getTags() {
+    const collectionRef  = collection(db, "posts");
+    const data = await getDocs(collectionRef);
+    const tags = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    return tags;
+}
+
+export default async function tags(){
+    const tags = await getTags();
+
     return(
-        <div className="flex-row place-items-center h-screen w-full">
+        <div className="flex-row md:px-64 px-5 place-items-center h-screen w-full">
             <h1 className="text-center text-2xl font-bold mt-12">
-                Search Article By Tag
+                All of the topics I've written about
             </h1>
+            <section className="grid grid-cols-8">
+                {tags.map((t: any) => (
+                    <Link href={`/tags/blog/${t.tag}`} className="btn btn-active btn-neutral w-[70%] bg-primary text-neutral hover:text-white btn-sm">{t.tag}</Link>
+                ))}
+            </section>
         </div>
     )
 }
